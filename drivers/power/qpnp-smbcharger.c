cb572f940dee414d2fb88060a82df8455cf94428
flar2 <asegaert@gmail.com>
2016-09-12 14:32:02 -0400

    USB fastcharge for kenzo
    Conflicts:
    	drivers/power/qpnp-smbcharger.c

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -39,10 +39,19 @@
 #include <linux/msm_bcl.h>
 #include <linux/ktime.h>
 #include "pmic-voter.h"
+<<<<<<< HEAD
 #include <linux/delay.h>
 #include <linux/wakelock.h>
 
 #define HVDCP_MANUAL_CURRENT_CONTROL 2700
+=======
+#include <linux/thermal.h>
+#include <linux/device.h>
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
+>>>>>>> 36ef5b4... USB fastcharge for kenzo
 
 /* Mask/Bit helpers */
 #define _SMB_MASK(BITS, POS) \
@@ -3583,6 +3592,10 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 	if (usb_supply_type != POWER_SUPPLY_TYPE_USB)
 		goto  skip_current_for_non_sdp;
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge)
+		current_limit = 900;
+#endif
 	pr_smb(PR_MISC, "usb type = %s current_limit = %d\n",
 			usb_type_name, current_limit);
 
