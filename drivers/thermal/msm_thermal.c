61978a5401a6c6cb6375d88a93c72e451badf4a1
chaubeyprateek <chaubeyprateek@gmail.com>
2016-11-16 07:32:24 +0300

    msm: thermal: Check temperature only if probed

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3347,7 +3347,10 @@ static void do_freq_control(long temp)
 static void check_temp(struct work_struct *work)
 {
 	long temp = 0;
-	int ret = 0;
+ 	int ret = 0;
+
+	if (!msm_thermal_probed)
+		return;
 
 	do_therm_reset();
 
